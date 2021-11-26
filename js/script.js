01ff8eb7f6ac073f8020047e4457dff91487e116
Your Name <hmilevskaalina7@gmail.com>
2021-11-26 15:24:25 +0200

    5th

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: false,
+        autoplaySpeed: 10000,
+        fade: true,
+        speed: 2000
+        });
+    $('.slider_second').slick({
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 1000,
+            autoplay: false,
+            autoplaySpeed: 10000,
+            fade: true,
+            speed: 2000
+            });
+        
+
+    
+
+});
