c66e487fb5ac563bb2754347fe2af6fea89ceed0
Your Name <hmilevskaalina7@gmail.com>
2021-12-07 16:57:53 +0200

    Swiper

--- js/script.js
+++ js/script.js
@@ -19,11 +19,65 @@ $(document).ready(function(){
             speed: 1000,
             autoplay: false,
             autoplaySpeed: 10000,
+       
             fade: true,
             speed: 2000
             });
-        
-
+    $('.slider_foto').slick({
+                arrows: false,
+                dots: false,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                speed: 1000,
+                infinite: false,
+            
+               
+             
+                responsive:[
+                    {
+                        breakpoint: 1590,
+                        settings: {
+                            slidesToShow:3
+                        }
+                    },
+                    {
+                        breakpoint: 1215,
+                        settings: {
+                            slidesToShow:2
+                        }
+                    
+                    },
+                    {
+                        breakpoint: 825,
+                        settings: {
+                            slidesToShow:1
+                        }
+                    
+                    }
+                       
+                ]
+              
+           
+                });
     
-
+});
+new Swiper('.image-slider',{
+    autoHeight: true,
+    slidesPerView: 4,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        820: {
+            slidesPerView: 2,
+        },
+        1210: {
+            slidesPerView: 3,
+        },
+        1620: {
+            slidesPerView: 4,
+        }
+        
+      
+    }
 });
