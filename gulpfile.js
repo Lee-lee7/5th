f58749f2c3e656d84f152cd72270aeaa7a8f0bd4
Your Name <hmilevskaalina7@gmail.com>
2021-12-10 16:12:38 +0200

    the header

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,44 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass')(require('node-sass'))
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const packageImporter = require('node-sass-package-importer');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
 
-var paths = {
-  styles: {
-    src: 'scss/**/*.scss',
-    dest: 'css'
-  }
-};
+const libraries = [
+  './node_modules/jquery/dist/jquery.js',
+  './node_modules/slick-carousel/slick/slick.js',
+  './node_modules/bootstrap/dist/js/bootstrap.js'
+]
+const browser = 'firefox' // 'default', 'google chrome', 'firefox'
 
-function styles() {
-  return gulp
-    .src(paths.styles.src, {
-      sourcemaps: true
-    })
-    .pipe(sass())
-    .pipe(gulp.dest(paths.styles.dest));
-}
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ importer: packageImporter() }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream({ match: '**/*.css' }));
+})
 
-function watch() {
-  gulp.watch(paths.styles.src, styles);
-}
+gulp.task('js', function () {
+  return gulp.src(libraries)
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify({ output: { comments: false, ascii_only: true, quote_keys: true } }))
+    .pipe(gulp.dest('./js'));
+})
 
-var build = gulp.parallel(styles, watch);
+gulp.task('watch', function () {
+  browserSync.init({
+    browser: browser,
+    server: { baseDir: './' }
+  });
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'))
+})
 
-
-gulp.task(build);
-gulp.task('default', build);
+gulp.task('default', gulp.series(gulp.parallel('js', 'sass'), 'watch'))
--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    
+$(function () {
+    let header = $('.header');
+    $('body').css('padding-top', header.height())
+
+    $(window).on('scroll', function (e) {
+        if (window.pageYOffset > 0) {
+            header.addClass('header_shadow');
+        } else {
+            header.removeClass('header_shadow');
+        }
+    })
+
     $('.slider').slick({
         arrows: false,
         dots: true,
@@ -10,54 +20,51 @@ $(document).ready(function(){
         autoplaySpeed: 10000,
         fade: true,
         speed: 2000
-        });
+    });
     $('.slider_second').slick({
-            arrows: false,
-            dots: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 1000,
-            autoplay: false,
-            autoplaySpeed: 10000,
-       
-            fade: true,
-            speed: 2000
-            });
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: false,
+        autoplaySpeed: 10000,
+
+        fade: true,
+        speed: 2000
+    });
     $('.slider_foto').slick({
-                arrows: false,
-                dots: false,
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                speed: 1000,
-                infinite: false,
-            
-               
-             
-                responsive:[
-                    {
-                        breakpoint: 1590,
-                        settings: {
-                            slidesToShow:3
-                        }
-                    },
-                    {
-                        breakpoint: 1215,
-                        settings: {
-                            slidesToShow:2
-                        }
-                    
-                    },
-                    {
-                        breakpoint: 825,
-                        settings: {
-                            slidesToShow:1
-                        }
-                    
-                    }
-                       
-                ]
-              
-           
-                });
-    
+        arrows: false,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        speed: 1000,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1590,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1215,
+                settings: {
+                    slidesToShow: 2
+                }
+
+            },
+            {
+                breakpoint: 825,
+                settings: {
+                    slidesToShow: 1
+                }
+
+            }
+
+        ]
+
+
+    });
+
 });
