59161ed3cc622b356153aefec7891abb76205228
Lee-lee7 <94048631+Lee-lee7@users.noreply.github.com>
2021-12-29 12:17:34 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,9 +17,11 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1000,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 10000,
-        fade: true,
+        fade: false,
+        infinite: false,
+        waitForAnimate: false,
         speed: 2000
     });
     $('.slider_second').slick({
@@ -30,42 +32,9 @@ $(function () {
         speed: 1000,
         autoplay: false,
         autoplaySpeed: 10000,
-
         fade: true,
         speed: 2000
     });
-    $('.slider_foto').slick({
-        arrows: false,
-        dots: false,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 1000,
-        infinite: false,
-        responsive: [
-            {
-                breakpoint: 1590,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 1215,
-                settings: {
-                    slidesToShow: 2
-                }
-
-            },
-            {
-                breakpoint: 825,
-                settings: {
-                    slidesToShow: 1
-                }
-
-            }
-
-        ]
-
-
-    });
+  
 
 });
